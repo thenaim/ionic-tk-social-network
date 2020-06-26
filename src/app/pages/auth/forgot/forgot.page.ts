e26174dc30be15fc3149dd5e1dd18f3d73271cd1
bossjon <bossjon.me@yandex.ru>
2020-06-26 21:59:04 +0300

    feat(forgot): add FormControl

--- src/app/pages/auth/forgot/forgot.page.ts
+++ src/app/pages/auth/forgot/forgot.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.page.scss'],
 })
 export class ForgotPage implements OnInit {
+  forgot: FormControl;
 
   constructor() { }
 
+  done() {
+    if (this.forgot.valid) {
+      console.log(this.forgot.value);
+    }
+  }
+
   ngOnInit() {
+    this.forgot = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
   }
 
 }
