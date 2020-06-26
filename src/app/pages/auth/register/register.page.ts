da8e461a64eb2a9bcb73b50ba3eb424a61844232
bossjon <bossjon.me@yandex.ru>
2020-06-26 21:59:27 +0300

    feat(register): add FormControl and terms

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  register: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
-  ngOnInit() {
+  onRegister() {
+    if (this.register.valid) {
+      console.log(this.register.value);
+    }
   }
 
+  ngOnInit() {
+    this.register = this.fb.group({
+      first_name: this.fb.control('', [
+        Validators.required,
+        Validators.maxLength(150)
+      ]),
+      last_name: this.fb.control('', [
+        Validators.required,
+        Validators.maxLength(150)
+      ]),
+      email: this.fb.control('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(150)
+      ]),
+      password_confirm: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(150)
+      ])
+    }, {
+      validators: this.passwordConfirmMatchValidator
+    });
+  }
+
+  passwordConfirmMatchValidator(g: FormGroup) {
+    const password = g.get('password');
+    const password_confirm = g.get('password_confirm');
+
+    if (password_confirm.hasError('required') || password_confirm.hasError('minlength')) return;
+
+    if (password.value !== password_confirm.value) {
+      password_confirm.setErrors({
+        mismatch: true
+      });
+    } else {
+      password_confirm.setErrors(null);
+    }
+  }
 }
