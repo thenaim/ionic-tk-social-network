bc57588cd75e13196d4bf78dfdccbfba499a6b6c
bossjon <bossjon.me@yandex.ru>
2020-06-29 16:46:15 +0300

    feat(explore): add menu navigation with translation

--- src/app/tabs/explore/explore.page.ts
+++ src/app/tabs/explore/explore.page.ts
@@ -6,7 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['explore.page.scss']
 })
 export class ExplorePage {
+  exps: any[] = [
+    { id: 'friends_title', icon: 'people-outline', color: 'danger', link: 'friends' },
+    { id: 'communities_title', icon: 'people-circle', color: 'warning', link: 'communities' },
+    { id: 'music_title', icon: 'musical-notes', color: 'primary', link: 'music' },
+    { id: 'stickers_title', icon: 'happy-outline', color: 'warning', link: 'stickers' },
+    { id: 'videos_title', icon: 'play-circle-outline', color: 'tertiary', link: 'videos' },
+    { id: 'live_streams_title', icon: 'ellipse-outline', color: 'warning', link: 'live_streams' },
+    { id: 'shopping_title', icon: 'cart-outline', color: 'warning', link: 'shopping' },
+    { id: 'podcasts_title', icon: 'headset-outline', color: 'dark', link: 'podcasts' }
+  ];
 
-  constructor() {}
+  constructor() { }
 
+  doRefresh(event) {
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 }
