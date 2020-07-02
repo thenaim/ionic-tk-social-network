d99487950df818ec26fde281d97131541ce94f88
bossjon <bossjon.me@yandex.ru>
2020-07-02 17:34:14 +0300

    feat(music): add music list

--- src/app/pages/music/music.page.ts
+++ src/app/pages/music/music.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FakerService } from '../../shared/faker/faker.service';
 
 @Component({
   selector: 'app-music',
@@ -10,9 +11,61 @@ export class MusicPage implements OnInit {
 
   searchMusic: FormControl = new FormControl('');
   activeSegment: FormControl = new FormControl('my_music');
-  constructor() { }
+
+  playlists: any[] = [];
+  musics: any[] = [];
+
+  constructor(
+    private fakerService: FakerService
+  ) { }
+
+  doRefresh(event) {
+    this.dataInit();
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 400);
+  }
+
+  dataInit() {
+    this.fakerService.getFaker().then((faker) => {
+      // Musics
+      this.musics = Array.apply(null, Array(8)).map(() => {
+        return {
+          id: faker.random.uuid(),
+          image: faker.helpers.randomize([
+            faker.image.city(500, 500),
+            faker.image.cats(500, 500),
+            faker.image.fashion(500, 500),
+            faker.image.people(500, 500)
+          ]),
+          song: faker.lorem.word(),
+          author: faker.internet.userName(),
+          time: faker.helpers.randomize([1, 2, 3, 4]) + ':' + faker.random.number(60)
+        };
+      });
+
+      // Playlist
+      this.playlists = Array.apply(null, Array(8)).map(() => {
+        return {
+          id: faker.random.uuid(),
+          image: faker.helpers.randomize([
+            faker.image.city(500, 500),
+            faker.image.cats(500, 500),
+            faker.image.fashion(500, 500),
+            faker.image.people(500, 500)
+          ]),
+          album: faker.lorem.word(),
+          author: faker.internet.userName(),
+          year: 2020
+        };
+      });
+    });
+  }
 
   ngOnInit() {
+    this.dataInit();
   }
 
 }
