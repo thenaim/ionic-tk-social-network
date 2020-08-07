0e975c6aae26f85d91c602d2679d8c82e00e59c2
thenaim <bossjon.me@yandex.ru>
2020-08-07 20:32:53 +0300

    feat(message-detail): add chat interface

--- src/app/pages/message-detail/message-detail.page.ts
+++ src/app/pages/message-detail/message-detail.page.ts
@@ -4,6 +4,7 @@ import { AppData } from 'src/app/providers/app-data';
 import { IonContent } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { debounceTime, tap } from 'rxjs/operators';
+import { FakerService } from 'src/app/shared/faker/faker.service';
 
 @Component({
   selector: 'app-message-detail',
@@ -15,9 +16,11 @@ export class MessageDetailPage implements OnInit {
   user = {};
   pageScrolling = false;
   scrolling: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  chats: any[] = [];
 
   @ViewChild(IonContent) private content: IonContent;
   constructor(
+    private faker: FakerService,
     private appData: AppData,
     private route: ActivatedRoute
   ) {
@@ -41,6 +44,30 @@ export class MessageDetailPage implements OnInit {
   }
 
   ngOnInit() {
+    this.faker.getFaker().then(faker => {
+      for (let ch = 0; ch < 6; ch++) {
+        const chat = {
+          date: faker.date.weekday(),
+          chats: []
+        };
+        
+        for (let ms = 0; ms < 12; ms++) {
+          chat.chats.push({
+            message: faker.lorem.sentences(faker.random.arrayElement([1, 2, 3])),
+            date: faker.date.recent(),
+            first_name: faker.name.findName(),
+            last_name: faker.name.lastName(),
+            avatar: faker.image.avatar(),
+            type: faker.random.arrayElement(['user', 'me'])
+          });
+        }
+
+        this.chats.push(chat);
+      }
+    });
+
+    console.log(this.chats);
+
     this.appData.getMessageUser(this.user_id).then((res) => {
       this.user = res;
       
