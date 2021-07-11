73106bd88354e55f4ef8cc9ce1d68bb140bde2fc
thenaim <bossjon.me@yandex.ru>
2021-07-12 02:50:47 +0300

    fix(): remove theme tag from ion-app tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { App } from '@capacitor/app';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
-import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
 import { selectIsDarkMode } from './pages/settings/settings.selectors';
 
 @Component({
@@ -11,15 +11,39 @@ import { selectIsDarkMode } from './pages/settings/settings.selectors';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  @Select(selectIsDarkMode()) isDarkMode$: Observable<boolean>;
-  constructor(private platform: Platform, private translateService: TranslateService) {
+export class AppComponent implements AfterViewInit {
+  constructor(private store: Store, private platform: Platform, private translateService: TranslateService) {
+    this.setupThemeListner();
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      SplashScreen.hide();
+      this.setupBackButtonListner();
+    });
+  }
+
+  setupThemeListner() {
+    this.store.select(selectIsDarkMode()).subscribe((isDarkMode: boolean) => {
+      if (isDarkMode) {
+        document.body.setAttribute('app-theme', 'dark');
+      } else {
+        document.body.removeAttribute('app-theme');
+      }
+    });
+  }
+
+  setupBackButtonListner() {
+    App.addListener('backButton', (data) => {
+      if (!data.canGoBack) {
+        App.exitApp();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.platform.ready().finally(() => {
+      setTimeout(() => SplashScreen.hide());
     });
   }
 }
